35d9d15d0f546af75e75e82335a6a81246f2df52
victoriabernadskiy <vbernadskiy0531@gmail.com>
2024-03-12 09:10:07 -0400

    api

--- vue-api-project/src/views/HomeView.vue
+++ vue-api-project/src/views/HomeView.vue
@@ -6,6 +6,18 @@
   </main>
 </template>
 
-<script>
+<script scoped>
+import TheWelcome from '../components/TheWelcome.vue'
 
+import{ref, onMounted} from 'vue';
+let toilets = ref('');
+async function getData() {
+  let res = await fetch("https://data.cityofnewyork.us/resource/hjae-yuav.json");
+  let data = await res.json();
+  console.log(data)
+  toilets.value= data;
+}
+onMounted(()=>{
+  getData()
+})
 </script>
