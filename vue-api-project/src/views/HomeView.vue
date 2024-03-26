9a696aca5a8dbae5f5b1ad60ed8cfc95936b4bc1
dennishvf <dennisk38@nycstudents.net>
2024-03-26 09:23:28 -0400

    .jhglji.uk

--- vue-api-project/src/views/HomeView.vue
+++ vue-api-project/src/views/HomeView.vue
@@ -6,33 +6,36 @@
 </template>
 
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+
 import BarChart from "../components/BarChart.vue"
 import{ref, onMounted} from 'vue';
-let toilets = ref('');
+let toilets = ref('getData');
 async function getData() {
   let res = await fetch("https://data.cityofnewyork.us/resource/hjae-yuav.json");
   let data = await res.json();
   console.log(data)
   toilets.value = data;
 }
-onMounted(()=>{
-  getData()
-})
-
-const text = {
+const valuee = {
   Brooklyn :0,
   StatenIsland :0,
-
+  Bronx :0,
+  Queens :0,
+  Manhattan :0,
 }
-toilets.value.forEach(element => {
+
+onMounted(()=>{
+  getData()
+  toilets.rawValue.forEach(element => {
   if (element.borough === "Brooklyn"){
-    
-    [text.Brooklyn]+1
+    [valuee.Brooklyn]+1
+    console.log(valuee)
   } 
 });
+})
 
-console.log(Brooklyn)
+console.log(toilets)
+
 </script>
 
 
