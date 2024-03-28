aaffd897d582ea3dade583f6900096cd6e6b7c04
dennishvf <dennisk38@nycstudents.net>
2024-03-28 09:28:56 -0400

    fm,xjkhdjn

--- vue-api-project/src/components/BarChart.vue
+++ vue-api-project/src/components/BarChart.vue
@@ -3,40 +3,46 @@
       id="my-chart-id"
       :options="chartOptions"
       :data="chartData"
+      v-if="loaded"
     />
   </template>
   
   <script >
   import { Bar } from 'vue-chartjs'
   import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
-  import{ref, onMounted} from 'vue';
-let toilets = ref('');
-async function getData() {
-  let res = await fetch("https://data.cityofnewyork.us/resource/hjae-yuav.json");
-  let data = await res.json();
-  console.log(data)
-  toilets.value = data;
-}
-onMounted(()=>{
-  getData()
-})
+ 
 
   ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
   
   export default {
     name: 'BarChart',
     components: { Bar },
+    props: {
+      info:Object,
+      test:Number
+    },
     data() {
       return {
         chartData: {
           labels: [ 'Brooklyn', 'Staten Island', 'Bronx', 'Queens', 'Manhattan' ],
-          datasets: [ { data: [0, 0, 0, 0, 0] } ]
+          datasets: [ { data: [this.test,0,0,0,0] } ]
         },
         chartOptions: {
           responsive: true
-        }
+        },
+        loaded :false
       }
     },
+    mounted: function(){
+      this.loaded = true
+     
+        
+        /* this.datasets[0].data.push(this.info[0]) 
+        console.log(this.info)
+        this.loaded = true */
+    
+    }
   }
+
   </script>
   
